92d85a8261297b76f7db5cfcbd83e4a45205c947
Uriel Aguilar <78172208+u-urieldev@users.noreply.github.com>
2023-01-18 15:09:11 -0600

    Carts ui ready

--- lib/screens/cards.dart
+++ lib/screens/cards.dart
@@ -25,10 +25,10 @@ class CardsPage extends StatelessWidget {
       onWillPop: () async => false,
       child: Scaffold(
         backgroundColor: Colors.white,
-        drawer: Menu(),
+        drawer: const Menu(),
         appBar: AppBar(
           backgroundColor: Colors.black,
-          iconTheme: IconThemeData(color: Colors.grey),
+          iconTheme: const IconThemeData(color: Colors.grey),
           title: TitleFont(
             text: "Your cards",
             size: 25,
@@ -45,7 +45,98 @@ class CardsPage extends StatelessWidget {
             ),
           ),
         ),
-        body: Center(),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GridView.count(
+            crossAxisCount: 2,
+            mainAxisSpacing: 6,
+            crossAxisSpacing: 6,
+            childAspectRatio: (.55),
+            children: List.generate(5, (index) {
+              return const CustomCard();
+            }),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomCard extends StatelessWidget {
+  const CustomCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => print('picado'),
+      child: Card(
+        elevation: 4,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        color: Color.fromARGB(255, 182, 180, 180),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 6,
+              child: ClipRRect(
+                borderRadius:
+                    const BorderRadius.vertical(top: Radius.circular(10)),
+                child: Image.network(
+                  'https://rickandmortyapi.com/api/character/avatar/452.jpeg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 4,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    flexText(
+                        'Simon',
+                        const TextStyle(
+                            fontSize: 23, fontWeight: FontWeight.w800)),
+                    const SizedBox(height: 7),
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.fiber_manual_record,
+                          size: 13,
+                          color: Colors.green,
+                        ),
+                        const SizedBox(width: 4),
+                        flexText(
+                            'Alive - Human', const TextStyle(fontSize: 15)),
+                      ],
+                    ),
+                    const SizedBox(height: 32),
+                    Row(
+                      children: [
+                        Icon(Icons.location_on_outlined),
+                        flexText(
+                            "Earth (replacement dimension", const TextStyle()),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Flexible flexText(String text, TextStyle style) {
+    return Flexible(
+      child: Text(
+        text,
+        overflow: TextOverflow.ellipsis,
+        style: style,
       ),
     );
   }
